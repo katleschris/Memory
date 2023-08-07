f0c7a5f7b007f945304609bdd341ff4ad3086a0d
katleschris <106029638+katleschris@users.noreply.github.com>
2023-08-07 23:38:46 +0200

    Initialised app and imported images

---
+++ matching-game/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import react from 'react';
+import cardArray from './images'
+
+function App() {
+
+ 
+
+  return (
+    <div className="App">
+     <h3>Score:<span id='result'></span></h3> 
+     <div className='grid'></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ matching-game/src/images.js
@@ -0,0 +1,84 @@
+export const cardArray = [
+    {
+      id : 1,
+      name: 'Lady in a hat',
+      img: 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/24/247559758eb20e46aabdff7e60e340133af1b544.jpg'
+    },
+    {
+        id : 1,
+        name: 'Lady in a hat',
+        img: 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/24/247559758eb20e46aabdff7e60e340133af1b544.jpg'
+      },
+    {
+      id: 2,
+      name: 'Cookie Chomper',
+      img: 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/4d/4d6f9f7cff5d2ace8a19393403d5410243a5acc6.jpg'
+    },
+    {
+        id: 2,
+        name: 'Cookie Chomper',
+        img: 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/4d/4d6f9f7cff5d2ace8a19393403d5410243a5acc6.jpg'
+      },
+    {
+      id: 3,
+      name: 'Royal Cow',
+      img: 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/63/631eb2abb25ce00dce6382a883c50c3a15e18d55.jpg'
+    },
+    {
+        id: 3,
+        name: 'Royal Cow',
+        img: 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/63/631eb2abb25ce00dce6382a883c50c3a15e18d55.jpg'
+      },
+    {
+      id: 4,
+      name: 'Kitty kiss',
+      img: 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/34/3463bc1122af17dcbea5999d0c10871465d597ea_full.jpg'
+    },
+    {
+        id: 4,
+        name: 'Kitty kiss',
+        img: 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/34/3463bc1122af17dcbea5999d0c10871465d597ea_full.jpg'
+      },
+    {
+      id: 5,
+      name: 'Bird log',
+      img: 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/82/8202024eda8810cb7cbb86fa438c63d293ab8faa_full.jpg'
+    },
+    {
+        id: 5,
+        name: 'Bird log',
+        img: 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/82/8202024eda8810cb7cbb86fa438c63d293ab8faa_full.jpg'
+      },
+    {
+      id: 6,
+      name: 'Pig man',
+      img: 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/5a/5ad9a3b809db0d722508842ba6b2f1532590277d_full.jpg'
+    },
+    {
+        id: 6,
+        name: 'Pig man',
+        img: 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/5a/5ad9a3b809db0d722508842ba6b2f1532590277d_full.jpg'
+      },
+    {
+      id: 7,
+      name: 'Naruto',
+      img: 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/00/00b11b65e89b3aa0ba286b5a27d771c83a5154db_full.jpg'
+    },
+    {
+        id: 7,
+        name: 'Naruto',
+        img: 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/00/00b11b65e89b3aa0ba286b5a27d771c83a5154db_full.jpg'
+      },
+    {
+      id:8,
+      name: 'Kermit',
+      img: 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/e3/e31afa6bc0a7d0a329a229c3547156598cd3e37d_full.jpg'
+    },
+    {
+        id:8,
+        name: 'Kermit',
+        img: 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/e3/e31afa6bc0a7d0a329a229c3547156598cd3e37d_full.jpg'
+      }
+
+
+  ];
