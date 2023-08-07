ed797088ae5e17074f769bf179c7eaf48652e1e3
katleschris <106029638+katleschris@users.noreply.github.com>
2023-08-08 00:11:45 +0200

    Created a flip card function

--- matching-game/src/App.js
+++ matching-game/src/App.js
@@ -1,8 +1,33 @@
 import './App.css';
 import react from 'react';
-import cardArray from './images'
+import {cardArray} from './images'
 
 function App() {
+  const grid = document.querySelector('.grid');
+  var cardsChosen = [];
+  var cardsChosenId = [];
+
+  function createBoard() {
+    
+    for (let i = 0; i < cardArray.length; i++) {
+      var card = document.createElement('img');
+      card.setAttribute('src', 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/00/003967ba0a28186d4c94b364a63e8647f25d39e3_full.jpg')
+      card.setAttribute('data-id', i)
+      grid.appendChild(card);
+
+    }
+  }
+  createBoard();
+
+  function flipCard(){
+    var cardId = this.getAttribute('data-id');
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute('src', cardArray[cardId].img);
+    if (cardsChosen.length ==2){
+      setTimeout(checkForMatch, 500)
+    }
+  }
 
  
 
