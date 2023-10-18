ead4a205a5062e0d1b69d9b72e47fe42b05c8392
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2023-10-18 11:42:09 +0200

    Cleaned up code

--- matching-game/src/App.js
+++ matching-game/src/App.js
@@ -1,36 +1,23 @@
-/**
- * React component representing a memory matching game.
- *
- * @component
- */
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { cardArray } from './images';
 import Background from './Background';
 import Fireworks from './Fireworks';
 
-/**
- * Represents the main application component.
- *
- * @returns {JSX.Element} The JSX element representing the App component.
- */
+// Constants for the default image and firework colors
+const backSrc = 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/74/74a89444d616571954a1e84780e94619ce79e848_full.jpg';
+const fireworkColors = ['#FF5733', '#33FF57', '#5733FF', '#FF33C7', '#33FFF4'];
+
 function App() {
-  const [cards, setCards] = useState([]); // Array of card objects
-  const [cardsChosen, setCardsChosen] = useState([]); // Array of chosen cards
-  const [cardsChosenId, setCardsChosenId] = useState([]); // Array of IDs of chosen cards
-  const [cardsWon, setCardsWon] = useState([]); // Array of cards that have been matched
-  const [difficultyLevel, setDifficultyLevel] = useState(3); // Current difficulty level
-  const [gridClass, setGridClass] = useState('easy'); // Current difficulty class
+  // State variables to manage the game
+  const [cards, setCards] = useState([]);
+  const [cardsChosen, setCardsChosen] = useState([]);
+  const [cardsChosenId, setCardsChosenId] = useState([]);
+  const [cardsWon, setCardsWon] = useState([]);
+  const [difficultyLevel, setDifficultyLevel] = useState(3);
+  const [gridClass, setGridClass] = useState('easy');
 
-  const backSrc = 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/74/74a89444d616571954a1e84780e94619ce79e848_full.jpg';
-
-  /**
-   * Create a card object.
-   *
-   * @param {string} imgUrl - The URL of the card image.
-   * @param {number} index - The unique identifier for the card.
-   * @returns {Object} The card object.
-   */
+  // Create a card object
   const createCard = (imgUrl, index) => {
     return {
       img: imgUrl,
@@ -38,19 +25,19 @@ function App() {
       id: index,
     };
   };
-  
+
+  // Options for different difficulty levels
   const difficultyOptions = [
     { value: 3, label: 'Easy' },
     { value: 6, label: 'Medium' },
     { value: 9, label: 'Hard' },
     { value: 12, label: 'Advanced' },
   ];
-  /**
-   * Create a new game board based on the current difficulty level.
-   */
+
+  // Create a new game board based on the current difficulty level
   const createBoard = () => {
     const selectedCards = cardArray.slice(0, difficultyLevel);
-    const duplicatedCards = [...selectedCards, ...selectedCards]; // Duplicate cards for pairs
+    const duplicatedCards = [...selectedCards, ...selectedCards];
     duplicatedCards.sort(() => 0.5 - Math.random());
     const newCards = duplicatedCards.map((card, index) => createCard(card.img, index));
     setCards(newCards);
@@ -65,27 +52,21 @@ function App() {
     } else if (difficultyLevel === 9) {
       newGridClass = 'hard';
     } else if (difficultyLevel === 12) {
-     newGridClass = 'advanced';
+      newGridClass = 'advanced';
     }
-
-  setGridClass(newGridClass);
+    setGridClass(newGridClass);
   };
 
   useEffect(() => {
     createBoard();
-  }, [difficultyLevel]); // Re-create board on difficulty change
+  }, [difficultyLevel]);
 
-  /**
-   * Flip a card when clicked.
-   *
-   * @param {number} cardId - The ID of the card being flipped.
-   */
+  // Flip a card when clicked
   const flipCard = (cardId) => {
     if (!cards[cardId].isFlipped && cardsChosen.length < 2) {
       const updatedCards = [...cards];
       updatedCards[cardId].isFlipped = true;
       setCards(updatedCards);
-
       setCardsChosen([...cardsChosen, updatedCards[cardId]]);
       setCardsChosenId([...cardsChosenId, cardId]);
     }
@@ -97,13 +78,10 @@ function App() {
     }
   }, [cardsChosen]);
 
-  /**
-   * Check if the chosen cards form a match.
-   */
+  // Check if the chosen cards form a match
   const checkForMatch = () => {
     const [optionOneId, optionTwoId] = cardsChosenId;
     if (cardsChosen[0].img === cardsChosen[1].img) {
-      // alert('You found a match');
       setCardsWon([...cardsWon, ...cardsChosen]);
     } else {
       const updatedCards = [...cards];
@@ -114,60 +92,46 @@ function App() {
     setCardsChosen([]);
     setCardsChosenId([]);
   };
- 
-  const fireworkColors = ['#FF5733', '#33FF57', '#5733FF', '#FF33C7', '#33FFF4'];
-
 
   return (
     <div className="app-container">
       <div className="app-content">
-        <h1>Test your Memory</h1>
-        <h3 className={`score ${cardsChosen.length === 2 && cardsChosen[0].img === cardsChosen[1].img ? 'matched' : ''}`}>
+        <h2>Test your Memory</h2>
+        <h5 className={`score ${cardsChosen.length === 2 && cardsChosen[0].img === cardsChosen[1].img ? 'matched' : ''}`}>
           Score: {cardsWon.length / 2}
-        </h3>
-
+        </h5>
         <div className="grid-top">
           <label className="label-spacing">Select Difficulty Level: </label>
-          <select  className="select-spacing" 
-            value={difficultyLevel}
-            onChange={(e) => setDifficultyLevel(parseInt(e.target.value))}
-
-          >
+          <select className="select-spacing" value={difficultyLevel} onChange={(e) => setDifficultyLevel(parseInt(e.target.value))}>
             {difficultyOptions.map((option) => (
               <option key={option.value} value={option.value}>
                 {option.label}
               </option>
             ))}
           </select>
-         
         </div>
         <div className={`grid ${gridClass}`}>
           {cards.map((card, index) => (
-          <img
-            key={index}
-            src={card.isFlipped ? card.img : backSrc}
-            alt="Card"
-            data-id={index}
-            onClick={() => flipCard(index)}
-          />
-        ))}
-      </div>
-      {cardsWon.length/2 === difficultyLevel && (
-        <div>
-          <p>Congratulations! You found them all!</p>
-          <Fireworks colors={fireworkColors} />
+            <img
+              key={index}
+              src={card.isFlipped ? card.img : backSrc}
+              alt="Card"
+              data-id={index}
+              onClick={() => flipCard(index)}
+            />
+          ))}
         </div>
-      )}
+        {cardsWon.length / 2 === difficultyLevel && (
+          <div>
+            <p>Congratulations! You found them all!</p>
+            <Fireworks colors={fireworkColors} />
+          </div>
+        )}
+      </div>
+      {cardsWon.length / 2 === difficultyLevel && <Fireworks colors={fireworkColors} />}
+      <Background />
     </div>
-    {cardsWon.length/2 === difficultyLevel && (
-          <Fireworks colors={fireworkColors} />
-      )}
-    <Background />
-    </div>
-    
   );
 }
 
 export default App;
-
-
